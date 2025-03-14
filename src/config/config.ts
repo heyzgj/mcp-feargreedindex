c6be19b0d60e196777dafbc2d71498b848c9556d
singularitygz <heyzgj@gmail.com>
2025-03-14 22:28:12 +0800

    bugs fixed

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // API configuration
-export const COINMARKETCAP_API_KEY = process.env.CMC_API_KEY || '5b338a09-fe63-4cba-85fa-7228191c8de8';
+export const COINMARKETCAP_API_KEY = process.env.CMC_API_KEY;
 export const FEAR_GREED_ENDPOINT = 'https://pro-api.coinmarketcap.com/v3/fear-and-greed/historical';
 
 // Server configuration
